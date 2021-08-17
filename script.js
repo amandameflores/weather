23fda8881acf0b35f54a333edfd27e19c97d4648
amandaflores <amandaflores.br@gmail.com>
2021-08-17 15:10:14 -0300

    api openweather url

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.querySelector('.busca').addEventListener('submit', (event)=>{
 
     if (input !== '') {
         showWarning('Loading...')
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ac3ea164e090ad6712e3138e6ce8362d`;
     }
 });
 
