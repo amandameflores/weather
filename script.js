48107e8becad2a403bf0bfa6fdbdb95173b6cd48
amandaflores <amandaflores.br@gmail.com>
2021-08-17 18:53:22 -0300

    clearInfo func before Loading

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
     let input = document.querySelector('#searchInput').value;
 
     if (input !== '') {
+        clearInfo();
         showWarning('Loading...')
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ac3ea164e090ad6712e3138e6ce8362d&units=metric&lang=pt-br`;
